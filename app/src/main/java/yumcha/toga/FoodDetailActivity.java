a33bfe63e74a2fb50bd82883c77c6610215687b6
azizimusa@azizi2u.com <Aswaja77>
2021-01-14 09:06:22 +0800

    implement parallax effect

--- app/src/main/java/yumcha/toga/FoodDetailActivity.java
+++ app/src/main/java/yumcha/toga/FoodDetailActivity.java
@@ -1,16 +1,8 @@
 package yumcha.toga;
 
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewPropertyAnimator;
-import android.widget.FrameLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,28 +10,13 @@ import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 
 import com.bumptech.glide.Glide;
-import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
-import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
-import com.github.ksoichiro.android.observablescrollview.ScrollState;
-import com.github.ksoichiro.android.observablescrollview.ScrollUtils;
 import com.google.gson.Gson;
-import com.nineoldandroids.view.ViewHelper;
 
-import timber.log.Timber;
 import yumcha.toga.databinding.FoodDetailBinding;
 import yumcha.toga.pojo.ListingPojo;
 
-public class FoodDetailActivity extends AppCompatActivity implements ObservableScrollViewCallbacks {
+public class FoodDetailActivity extends AppCompatActivity {
 
-    private static final float MAX_TEXT_SCALE_DELTA = 0.3f;
-
-    private View mImageView;
-    private View mOverlayView;
-    private ObservableScrollView mScrollView;
-    private TextView mTitleView;
-    private int mActionBarSize;
-    private int mFlexibleSpaceShowFabOffset;
-    private int mFlexibleSpaceImageHeight;
     FoodDetailBinding view;
 
     @Override
@@ -48,52 +25,20 @@ public class FoodDetailActivity extends AppCompatActivity implements ObservableS
         view = DataBindingUtil.setContentView(this, R.layout.food_detail);
         view.setVm(this);
 
-        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar().setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.toolbar_color));
-        getSupportActionBar().setHomeButtonEnabled(true);
+        setSupportActionBar(view.toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         String pojo = getIntent().getStringExtra("food");
         ListingPojo listingPojo = new Gson().fromJson(pojo, ListingPojo.class);
 
         loadContent(listingPojo);
 
-        mFlexibleSpaceImageHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_image_height);
-        mFlexibleSpaceShowFabOffset = getResources().getDimensionPixelSize(R.dimen.flexible_space_show_fab_offset);
-        mActionBarSize = getSupportActionBar().getHeight();
-
-        mImageView = findViewById(R.id.image);
-        mOverlayView = findViewById(R.id.overlay);
-        mScrollView = (ObservableScrollView) findViewById(R.id.scroll);
-        mScrollView.setScrollViewCallbacks(this);
-        mTitleView = (TextView) findViewById(R.id.title);
-//        mTitleView.setText(getTitle());
-        setTitle(null);
-
-        ScrollUtils.addOnGlobalLayoutListener(mScrollView, new Runnable() {
-            @Override
-            public void run() {
-                mScrollView.scrollTo(0, mFlexibleSpaceImageHeight - mActionBarSize);
-
-                Timber.e("yooooo");
-
-                // If you'd like to start from scrollY == 0, don't write like this:
-                //mScrollView.scrollTo(0, 0);
-                // The initial scrollY is 0, so it won't invoke onScrollChanged().
-                // To do this, use the following:
-                //onScrollChanged(0, false, false);
-
-                // You can also achieve it with the following codes.
-                // This causes scroll change from 1 to 0.
-                //mScrollView.scrollTo(0, 1);
-                //mScrollView.scrollTo(0, 0);
-            }
-        });
     }
 
     private void loadContent(ListingPojo listingPojo) {
 
-        view.title.setText(listingPojo.getProductName());
+        getSupportActionBar().setTitle(listingPojo.getProductName());
         view.subtitle.setText(listingPojo.getProductDesc());
         view.star.setText(String.valueOf(listingPojo.getStar()));
         view.distance.setText(listingPojo.getDistance());
@@ -112,57 +57,6 @@ public class FoodDetailActivity extends AppCompatActivity implements ObservableS
         }
 
         Glide.with(this).load(listingPojo.getImageUrl()).into(view.image);
-    }
-
-    @Override
-    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
-        // Translate overlay and image
-        float flexibleRange = mFlexibleSpaceImageHeight - mActionBarSize;
-        int minOverlayTransitionY = mActionBarSize - mOverlayView.getHeight();
-        ViewHelper.setTranslationY(mOverlayView, ScrollUtils.getFloat(-scrollY, minOverlayTransitionY, 0));
-        ViewHelper.setTranslationY(mImageView, ScrollUtils.getFloat(-scrollY / 2, minOverlayTransitionY, 0));
-
-        // Change alpha of overlay
-        ViewHelper.setAlpha(mOverlayView, ScrollUtils.getFloat((float) scrollY / flexibleRange, 0, 1));
-
-        // Scale title text
-        float scale = 1 + ScrollUtils.getFloat((flexibleRange - scrollY) / flexibleRange, 0, MAX_TEXT_SCALE_DELTA);
-        ViewHelper.setPivotX(mTitleView, 0);
-        ViewHelper.setPivotY(mTitleView, 0);
-        ViewHelper.setScaleX(mTitleView, scale);
-        ViewHelper.setScaleY(mTitleView, scale);
-
-        // Translate title text
-        int maxTitleTranslationY = (int) (mFlexibleSpaceImageHeight - mTitleView.getHeight() * scale);
-        int titleTranslationY = maxTitleTranslationY - scrollY;
-        ViewHelper.setTranslationY(mTitleView, titleTranslationY);
-
-//        // Translate FAB
-//        int maxFabTranslationY = mFlexibleSpaceImageHeight - mFab.getHeight() / 2;
-//        float fabTranslationY = ScrollUtils.getFloat(
-//                -scrollY + mFlexibleSpaceImageHeight - mFab.getHeight() / 2,
-//                mActionBarSize - mFab.getHeight() / 2,
-//                maxFabTranslationY);
-//        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
-//            // On pre-honeycomb, ViewHelper.setTranslationX/Y does not set margin,
-//            // which causes FAB's OnClickListener not working.
-//            FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) mFab.getLayoutParams();
-//            lp.leftMargin = mOverlayView.getWidth() - mFabMargin - mFab.getWidth();
-//            lp.topMargin = (int) fabTranslationY;
-//            mFab.requestLayout();
-//        } else {
-//            ViewHelper.setTranslationX(mFab, mOverlayView.getWidth() - mFabMargin - mFab.getWidth());
-//            ViewHelper.setTranslationY(mFab, fabTranslationY);
-//        }
-
-    }
-
-    @Override
-    public void onDownMotionEvent() {
-    }
-
-    @Override
-    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
     }
 
     @Override
